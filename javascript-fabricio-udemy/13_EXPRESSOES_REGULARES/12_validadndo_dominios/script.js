0442d1358f1529e669a2265e0644f2d42d5c379c
FabricioViannaSM <fabricioviannad2@gmail.com>
2023-03-09 12:57:23 -0300

    commit manutenção 09/03/2023

--- javascript-fabricio-udemy/13_EXPRESSOES_REGULARES/12_validadndo_dominios/script.js
+++ javascript-fabricio-udemy/13_EXPRESSOES_REGULARES/12_validadndo_dominios/script.js
@@ -1,6 +1,6 @@
-const dominio = /:www.\w+.com|com.br/ //em manutenção
+const dominio = /www.\w+.com|com.br/
 
 console.log(dominio.test("www.sitealeatorio.com"))
 console.log(dominio.test("www.sitealeatorio.com.br"))
-console.log(dominio.test("www.sitealeatorio.com"))
+console.log(dominio.test("ww.sitealeatorio.com"))
 console.log(dominio.test("www.sitealeatorio.co"))
