58c64a9909152bbfa5005c3f171074fae1d89022
FabricioViannaSM <fabricioviannad2@gmail.com>
2023-06-14 18:26:12 -0300

    commit 14/06 part 2

--- javascript-fabricio-udemy/13_EXPRESSOES_REGULARES/11_choice_pattern/script.js
+++ javascript-fabricio-udemy/13_EXPRESSOES_REGULARES/11_choice_pattern/script.js
@@ -1,5 +1,7 @@
-let doces = /\d (balas|pirulitos|cocadas)/ // funciona como "ou"
+let veiculos = /\d (carros|motos|lanchas|jet skis)/ // funciona como o operador "ou"
 
-console.log(doces.test("10 balas"))
-console.log(doces.test("21 pirulitos"))
-console.log(doces.test("45 bananadas"))
+console.log(veiculos.test("10 motos"))
+console.log(veiculos.test("15 carros"))
+console.log(veiculos.test("3 jet skis"))
+console.log(veiculos.test("9 lanchas"))
+console.log(veiculos.test("3 bikes"))
