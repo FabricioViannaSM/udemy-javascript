6d7a61e0e0fac9d87761cd8759dbf410dcbb8deb
FabricioViannaSM <fabricioviannad2@gmail.com>
2023-06-14 18:11:25 -0300

    commit 14/06 part 1

--- javascript-fabricio-udemy/13_EXPRESSOES_REGULARES/8_ocorrencia_precisa/script.js
+++ javascript-fabricio-udemy/13_EXPRESSOES_REGULARES/8_ocorrencia_precisa/script.js
@@ -1,20 +1,20 @@
-let tel = /\d{4,5}-?\d{4}/  //deve conter 4 ou 5 digitos, pode ou nao conter "-", deve conter 4 digitos
+let cel = /\d{4,5}-\d{4}/ // delimita caracteres precisos
 
-console.log(tel.test("99999-9999"))
-console.log(tel.test("9999-9999"))
-console.log(tel.test("999-9999"))
-console.log(tel.test("999999999"))
-console.log(tel.test("99999999"))
-console.log(tel.test("9999999"))
+console.log("celular")
+console.log(cel.test("12345-6789"))
+console.log(cel.test("1234-5678"))
+console.log(cel.test("123-4567"))
+console.log(cel.test("1234-567"))
 
-let cpf = /\d{3}.\d{3}.\d{3}-\d{2}/
+let rg = /\d{2}.\d{3}.\d{3}-\d{1}/
 
-console.log("\nCPF:")
-console.log(cpf.test("111.111.111-11"))
-console.log(cpf.test("123.123.123.12"))
+console.log("\nRG:")
+console.log(rg.test("28.234.883-0"))
+console.log(rg.test("123.123.123.12"))
 
-let data = /\d{2}\/\d{2}\/\d{4}/ //Para utilizar carcteres especiais usa-se "\" na frente deles
-
+let data = /\d{2}\/\d{2}\/\d{4}/
 
 console.log("\nData:")
-console.log(data.test("08/03/2023"))
+console.log(data.test("02/06/2024"))
+console.log(data.test("23/04/22"))
+console.log(data.test("23/05/123"))
--- javascript-fabricio-udemy/13_EXPRESSOES_REGULARES/9_exec/script.js
+++ javascript-fabricio-udemy/13_EXPRESSOES_REGULARES/9_exec/script.js
@@ -1,5 +1,5 @@
-let num = /\d+/
+let space = /\s/
 
-console.log(num.exec("Com o número 100"))
-console.log(num.exec("Com o número 100").index)// retorna o index de 100
-console.log(num.exec("Não tem número"))// retorna null
+console.log(space.exec("Com espaço"))
+console.log(space.exec("Com espaço").index)// retorna apenas o index dp espaço
+console.log(space.exec("semEspaço"))// retorna null
