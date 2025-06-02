6ac13e10f36901dae58f1a49cb495a2151cf3ae0
Fabrício Vianna <104432845+FabricioViannaSM@users.noreply.github.com>
2025-06-02 17:03:29 -0300

    commit 29/30

---
+++ javascript-fabricio-udemy/2_tipos-de-dados/8_boolean_operadores/script.js
@@ -0,0 +1,13 @@
+//&&
+console.log(10 > 1 && 1 < 11);
+console.log(10 < 1 && 1 > 11);// se ambos forem false não funciona
+console.log(0 >= 1 && 1>=0)
+
+//||
+console.log(10 > 1 || 1 < 11);
+console.log(10 < 1 || 1 > 11);
+console.log(0 >= 1 || 1>=0)
+
+//!
+console.log(!(10 != 10));
+console.log(!("Fabrício" == 'Fabrício'))
