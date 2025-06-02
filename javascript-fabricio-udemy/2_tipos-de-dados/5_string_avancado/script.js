f8024301306ca273e7cfc89967faa4133c1fbb8b
Fabrício Vianna <104432845+FabricioViannaSM@users.noreply.github.com>
2025-06-02 15:54:04 -0300

    commit 25

---
+++ javascript-fabricio-udemy/2_tipos-de-dados/5_string_avancado/script.js
@@ -0,0 +1,4 @@
+console.log('Tenho Você mas o meu medo "estraga"');
+console.log("O medo " + "se vai," + "quando ");
+console.log("Ouço a voz do alto a me dizer: \n SÊ VALENTE");
+console.log(`A soma de 2 + 2 é igual a ${2+2}`);
