c03e7c28ea3fc62f2e82eb2347a8cecf164aaf80
Fabrício Vianna <104432845+FabricioViannaSM@users.noreply.github.com>
2025-06-03 17:07:25 -0300

    commit 59

---
+++ javascript-fabricio-udemy/3_estruturas_de_programacao/9_if/script.js
@@ -0,0 +1,14 @@
+let idade = 20
+let nome = "Fabrício"
+
+if(nome == "Fabrício"){
+    console.log(`Olá, meu nome é Fabrício e tenho ${idade} anos`)
+}
+
+if(idade >= 18){
+    console.log("Você é maior de idade")
+}
+
+if(nome == "João" || idade == 20){
+    console.log("Ou seu nome é João, ou sua idade é igual a 20 anos")
+}
