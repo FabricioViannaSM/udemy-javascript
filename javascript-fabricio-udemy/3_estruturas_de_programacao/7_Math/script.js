a8ce8f31546da7274945f2024690ebb940632ed0
Fabrício Vianna <104432845+FabricioViannaSM@users.noreply.github.com>
2025-06-03 15:37:30 -0300

    commit 56

---
+++ javascript-fabricio-udemy/3_estruturas_de_programacao/7_Math/script.js
@@ -0,0 +1,9 @@
+let maiorNumero = Math.max(1,2,3,4,5,6,7,7,3,4,5)
+let menorNumero = Math.min(1,2,3,4,5,6,7,7,3,4,5)
+let raizQuadrada = Math.sqrt(100)
+let potencia = Math.pow(10, 2)
+
+console.log(maiorNumero)
+console.log(menorNumero)
+console.log(raizQuadrada)
+console.log(potencia)
