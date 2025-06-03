96db7ba78093e043c569f06bd5329b1d16411701
Fabrício Vianna <104432845+FabricioViannaSM@users.noreply.github.com>
2025-06-03 11:20:37 -0300

    commit 48

---
+++ javascript-fabricio-udemy/3_estruturas_de_programacao/1_variaveis/script.js
@@ -0,0 +1,8 @@
+let nome = "Fabrício";
+let idade = 20, altura = 1.86;
+
+console.log(`Olá meu nome é ${nome}, tenho ${idade} anos de idade e ${altura} metros de altura!`);
+
+idade += 1
+
+console.log(`Ainda este ano farei ${idade} anos de idade!`)
