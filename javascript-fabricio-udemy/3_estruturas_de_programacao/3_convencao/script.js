09ee5069fcd09e7af0ed46a49f546145b23d3fac
Fabrício Vianna <104432845+FabricioViannaSM@users.noreply.github.com>
2025-06-03 14:30:10 -0300

    commit 50

---
+++ javascript-fabricio-udemy/3_estruturas_de_programacao/3_convencao/script.js
@@ -0,0 +1,15 @@
+// não pode usar no começo
+//let 1nome = "Salamandra" 
+
+//Pode usar número depois
+let nome1= "Salamandra";
+
+//Pode começar com letra maiúscula
+let Nome = "Salamandra";
+
+//Pode usar $ ou _
+let _nome = "Salamandra";
+let $nome = "Salamandra";
+
+//O Mais indicardo é usar o camelCase
+let camelCase = "Camelo";
