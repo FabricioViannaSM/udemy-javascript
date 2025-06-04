908905e084cdf3e652927121b3501d9344ec04ce
Fabrício Vianna <104432845+FabricioViannaSM@users.noreply.github.com>
2025-06-04 19:30:03 -0300

    commit 72

---
+++ javascript-fabricio-udemy/3_estruturas_de_programacao/20_comentario/script.js
@@ -0,0 +1,21 @@
+//Comentário de uma linha:
+
+//let nome = "Ben";
+
+/*
+    Comentário de mais de uma linha:
+*/
+
+/*
+switch(nome){
+    case "Aragão":
+        console.log("Bem vindo, Aragão")
+        break;
+    case "Ben":
+        console.log("Bem vindo, Ben!");
+        break;
+    default:
+        console.log("Olá, você deve ser o Jorge!")
+
+}
+*/
