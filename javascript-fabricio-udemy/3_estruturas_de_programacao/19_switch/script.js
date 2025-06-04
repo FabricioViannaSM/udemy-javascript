dead174ca172cd510841eb9fcadf3a1e3e484959
Fabrício Vianna <104432845+FabricioViannaSM@users.noreply.github.com>
2025-06-04 19:19:39 -0300

    commit 70

---
+++ javascript-fabricio-udemy/3_estruturas_de_programacao/19_switch/script.js
@@ -0,0 +1,13 @@
+let nome = "Ben";
+
+switch(nome){
+    case "Aragão":
+        console.log("Bem vindo, Aragão")
+        break;
+    case "Ben":
+        console.log("Bem vindo, Ben!");
+        break;
+    default:
+        console.log("Olá, você deve ser o Jorge!")
+
+}
