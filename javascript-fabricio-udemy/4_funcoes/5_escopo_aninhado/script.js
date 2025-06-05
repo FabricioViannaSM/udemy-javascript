ff246d155a1ee7e5ac8f3bad6a6c90e570dee948
Fabrício Vianna <104432845+FabricioViannaSM@users.noreply.github.com>
2025-06-05 14:58:18 -0300

    commit 106

---
+++ javascript-fabricio-udemy/4_funcoes/5_escopo_aninhado/script.js
@@ -0,0 +1,14 @@
+let x = 10;
+
+function x111(n){
+    let x = 111
+    let y = n*x
+        if(y == 111){
+            let x =111
+            return`Você multiplicou ${x} por 1`
+        }
+}
+
+console.log(x111(1))
+
+console.log(x)
