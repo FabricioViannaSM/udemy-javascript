402e50e4f4c43875a78af11073982e512c3e6331
Fabrício Vianna <104432845+FabricioViannaSM@users.noreply.github.com>
2025-06-09 17:37:16 -0300

    commit 145

---
+++ javascript-fabricio-udemy/5_arrays_objetos/10_loops_arrays/script.js
@@ -0,0 +1,13 @@
+let numeros = [0,1,2,3,4,5,6,7,8,9,10]
+
+for(let i =0; i< numeros.length; i++){
+    console.log(numeros[i])
+}
+
+    let i = 0
+
+while(i<numeros.length){
+
+    console.log(numeros[i])
+    i++
+}
