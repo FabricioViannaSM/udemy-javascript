5316441e1b30426ba9a52f89ef68048aff62bcec
Fabrício Vianna <104432845+FabricioViannaSM@users.noreply.github.com>
2025-06-09 15:33:49 -0300

    commit 140

---
+++ javascript-fabricio-udemy/5_arrays_objetos/5_objetos/script.js
@@ -0,0 +1,12 @@
+let carro = {
+    marca: "Mitsubishi",
+    modelo: "Lancer Evolution 4",
+    ano: 1996,
+    darPartida: function(){
+        console.log("vrumm vrummmm")
+    }
+}
+
+console.log(carro.modelo)
+console.log(carro.ano)
+carro.darPartida()
