dfce45a687535b9b1a664188c2d1b8689ee5a3b9
Fabrício Vianna <104432845+FabricioViannaSM@users.noreply.github.com>
2025-06-09 15:57:23 -0300

    commit 142

---
+++ javascript-fabricio-udemy/5_arrays_objetos/7_copiar_objetos/script.js
@@ -0,0 +1,14 @@
+let objeto1 = {
+    proptiedade1: true,
+    propriedade2: "Sim"
+}
+
+let objeto2 = {
+    propriedade3: 10
+}
+
+console.log(objeto1)
+
+Object.assign(objeto1, objeto2)
+
+console.log(objeto1)
