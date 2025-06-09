4ffc58267359b34e2c712833b2a9f00fe91f606a
Fabrício Vianna <104432845+FabricioViannaSM@users.noreply.github.com>
2025-06-09 14:53:36 -0300

    commit 137

---
+++ javascript-fabricio-udemy/5_arrays_objetos/2_mais_sobre_array/script.js
@@ -0,0 +1,12 @@
+let number = [1 , 2, 3, 4, 5, 6, 7, 8, 9, 10]
+let string = ["Mateus", "Marcos", "Lucas", "João"]
+let boolean = [true, false] 
+let mix = [1, "Fabrício", true]
+
+numero2 = number[1]
+joão = string[3]
+ultimoNumero = number[number.length - 1]
+
+console.log(numero2)
+console.log(joão)
+console.log(ultimoNumero)
