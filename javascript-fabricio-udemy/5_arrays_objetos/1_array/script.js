b9e5ec638bb878a1b15a1fd341f08bddfeea95f8
Fabrício Vianna <104432845+FabricioViannaSM@users.noreply.github.com>
2025-06-09 11:27:07 -0300

    commit 136

---
+++ javascript-fabricio-udemy/5_arrays_objetos/1_array/script.js
@@ -0,0 +1,9 @@
+let number = [1 , 2, 3, 4, 5, 6, 7, 8, 9, 10]
+let string = ["Mateus", "Marcos", "Lucas", "João"]
+let boolean = [true, false] 
+let mix = [1, "Fabrício", true]
+
+console.log(number)
+console.log(string)
+console.log(boolean)
+console.log(mix)
