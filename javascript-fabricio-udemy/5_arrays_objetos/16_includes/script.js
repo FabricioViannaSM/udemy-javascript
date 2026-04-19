d2beee800139eb4296ee3fd2bd8311f9419d3377
Fabrício Vianna <104432845+FabricioViannaSM@users.noreply.github.com>
2026-04-19 20:15:05 -0300

    commit 151

---
+++ javascript-fabricio-udemy/5_arrays_objetos/16_includes/script.js
@@ -0,0 +1,10 @@
+const nums = [1, 2, 3, 4, 5, 6]
+
+console.log(nums.includes(5))
+console.log(nums.includes(7))
+
+const dj = ["Monster", "com", "leite"]
+
+if(dj.includes("leite")){
+    console.log("Hmm, então contém leite")
+}
